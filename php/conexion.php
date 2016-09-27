04d2c8b7e21053ad3944f288dfbdbafb1d2dfd82
Moisés Godoy <mgodoy@dl.cl>
2016-09-26 23:23:09 -0300

    se agregar docker

--- php/conexion.php
+++ php/conexion.php
@@ -1,12 +1,13 @@
 <?php
 
-$server = "127.0.0.1";
-$usuario ="root";
-$contraseña ="lentao1649_";
-$bd = "prueba";
+$host = "database";
+$user ="root";
+$password ="123";
+$database = "test";
+$port = 3306;
 
-$conexion = mysqli_connect($server,$usuario,$contraseña,$bd)
-			or die ("error en la conexion");
+//$conexion = mysqli_connect($server,$usuario,$contraseña,$bd)
 
+$db_link = mysqli_connect( $host, $user, $password, $database, $port ) or die ("error en la conexion");
 
 ?>
